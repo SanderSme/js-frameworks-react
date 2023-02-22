fe989d99be5ff2a17265cdde1570a97dfe6b345c
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2023-02-22 12:05:51 +0100

    cra

---
+++ day-1-start/src/components/Heading.js
@@ -0,0 +1,9 @@
+function Heading() {
+    return(
+    <div>
+        <h1>XanderZone</h1>
+    </div>
+    )
+}
+
+export default Heading;
---
+++ day-1-start/src/components/List.js
@@ -0,0 +1,11 @@
+function List() {
+    return(
+      <ul>
+        <li>American Football</li>
+        <li>Travelling</li>
+        <li>History</li>
+      </ul>
+    )
+}
+
+export default List;
